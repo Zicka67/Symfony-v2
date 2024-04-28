e022c2910e8c9644cf9344912ae6dbb3b83514b1
Zicka67 <art.book@hotmail.fr>
2024-04-29 01:25:33 +0200

    Mise en place d'un carrousel et modification en JS pour afficher un text différent a chaque slide

--- symfonyv2/public/js/script.js
+++ symfonyv2/public/js/script.js
@@ -4,11 +4,36 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const nextBtn = document.getElementById('nextBtn');
     const slide = document.querySelector('.carousel-slide');
     const images = document.querySelectorAll('.carousel-slide img');
+    
+    const textMain = document.getElementById("carousel-text-main");
+    const textsMain = [
+      "Vente flash d'avril", 
+      "Nouveau beurre de cacahuète", 
+      "Le plein de protéines, un pur plaisir"
+    ];
+
+    const text = document.getElementById("carousel-text-secondary");
+    const texts = [
+      "Du 25 au 28 avril, profite de 25% sur tous les produits de notre site.", 
+      "Nouveau beurre de Cacachuète CROQUANT ET SALE: délicieux et rempli de protéine.", 
+      "Vos besoins journaliers en protéines avec notre poudre de lactosérum."
+    ];
+ 
+    function updateText() {
+      text.textContent = texts[counter];
+    }
+
+    function updateTextMain() {
+      textMain.textContent = textsMain[counter];
+    }
+    
   
     let counter = 0;
     const size = images[0].clientWidth;
-    let autoSlideInterval = setInterval(moveToNextSlide, 3000); 
+    let autoSlideInterval = setInterval(moveToNextSlide, 8000); 
     let direction = 1; // 1 pour avancer, -1 pour reculer
+
+   
   
     function moveToNextSlide() {
       if (counter >= images.length - 1) { // Si on est à la dernière image
@@ -17,9 +42,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
         direction = 1; // Réinitialise la direction pour avancer
       }
   
-      slide.style.transition = "transform 0.4s ease-in-out";
+      slide.style.transition = "transform 1s ease-in-out";
       counter += direction;
       slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+      updateText();
+      updateTextMain(); // Met à jour le texte à chaque transition
     }
   
     function stopAutoSlide() {
@@ -29,6 +57,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
     nextBtn.addEventListener('click', () => {
       stopAutoSlide(); // Arrête le défilement automatique lorsque l'utilisateur interagit
       moveToNextSlide();
+      updateText();
+      updateTextMain();
     });
   
     prevBtn.addEventListener('click', () => {
@@ -37,6 +67,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
       counter--;
       slide.style.transition = "transform 0.4s ease-in-out";
       slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+      updateText();
+      updateTextMain();
     });
   
   });
