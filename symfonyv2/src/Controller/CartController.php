1e5816beb313612dc4636a9e9f4a11c29a954e5b
Zicka67 <art.book@hotmail.fr>
2024-04-29 20:45:57 +0200

    Modification du cartcontroller pour intégrer l'application des coupons

--- symfonyv2/src/Controller/CartController.php
+++ symfonyv2/src/Controller/CartController.php
@@ -2,34 +2,89 @@
 
 namespace App\Controller;
 
+use App\Repository\CouponRepository;
 use App\Repository\ProductsRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CartController extends AbstractController
 {
 
-    #[Route('/cart', name: 'cart_show')]
-    public function showCart(SessionInterface $session, ProductsRepository $productsRepository): Response
+    #[Route('/cart', name: 'cart_show', methods: ['GET', 'POST'])]
+public function show(SessionInterface $session, ProductsRepository $productsRepository, Request $request, CouponRepository $couponRepository ): Response
+{
+    $cart = $session->get('cart', []);
+    
+    $cartWithData = [];
+    $total = $this->calculateCartTotal($session, $productsRepository);
+    // dd($total);
+    $deliveryCost = 4.50; 
+
+    foreach ($cart as $id => $quantity) {
+        $cartWithData[] = [
+            'product' => $productsRepository->find($id),
+            'quantity' => $quantity
+        ];
+    }
+
+    // Vérification pour le code du coupon
+    $couponCode = $request->request->get('coupon_code');
+
+   
+    if ($couponCode) {
+        $coupon = $couponRepository->findOneBy(['code' => $couponCode, 'is_valid' => true]);
+        if ($coupon) {
+            $discountValue = $total * ($coupon->getDiscount() / 100);
+
+            $total -= $discountValue;
+            // Enregistrer le coupon et la nouvelle valeur totale dans la session
+            $session->set('cart_total_with_discount', $total);
+            $session->set('coupon', $couponCode);
+        } else {
+            // Ajouter un message flash pour indiquer que le coupon est invalide
+            // $this->addFlash('danger', 'Le coupon n\'est pas valide.');
+        }
+        
+    }
+    // var_dump($request->isXmlHttpRequest());
+    $cartTotalWithDiscount = $session->get('cart_total_with_discount') ?? $total;
+    // var_dump($request->isXmlHttpRequest());
+
+    // Si la requête est une requête AJAX, renvoie uniquement le prix total mis à jour en réponse JSON
+    if ($request->isXmlHttpRequest()) {
+        $response = new JsonResponse();
+        $response->setData([
+            'total' => $cartTotalWithDiscount
+        ]);
+        return $response;
+    }
+
+    // Sinon, continuez avec le rendu de la vue normalement
+    return $this->render('cart/index.html.twig', [
+        'items' => $cartWithData,
+        'total' => $total,
+        'cartTotalWithDiscount' => $cartTotalWithDiscount,
+        'deliveryCost' => $deliveryCost 
+    ]);
+}
+
+    private function calculateCartTotal(SessionInterface $session, ProductsRepository $productsRepository)
     {
         $cart = $session->get('cart', []);
-        $cartWithData = [];
+        $total = 0;
 
         foreach ($cart as $id => $quantity) {
             $product = $productsRepository->find($id);
             if ($product) {
-                $cartWithData[] = [
-                    'product' => $product,
-                    'quantity' => $quantity
-                ];
+                $total += ($product->getPrice() * $quantity);
             }
         }
 
-        return $this->render('cart/index.html.twig', [
-            'items' => $cartWithData
-        ]);
+        return $total;
     }
 
     #[Route('/cart/add/{id}', name: 'cart_add')]
@@ -113,7 +168,29 @@ class CartController extends AbstractController
         return $this->redirectToRoute('cart_show');
     }
 
-   
+    // #[Route('/cart/apply-coupon', name: 'cart_apply_coupon', methods: ['POST'])]
+    // public function applyCoupon(Request $request, SessionInterface $session, ProductsRepository $productsRepository, CouponRepository $couponRepository): Response
+    // {
+    //     $couponCode = $request->request->get('coupon_code');
+    //     $coupon = $couponRepository->findOneBy(['code' => $couponCode, 'is_valid' => true]);
+        
+    //     if (!$coupon) {
+    //         $this->addFlash('danger', 'Le coupon n\'est pas valide.');
+    //         return $this->redirectToRoute('cart_show');
+    //     }
+
+    //     $total = $this->calculateCartTotal($session, $productsRepository);
+    //     $discountValue = $total * ($coupon->getDiscount() / 100);
+    //     $newTotal = $total - $discountValue;
+
+    //     $session->set('cart_total_with_discount', $newTotal);
+    //     $session->set('coupon', $couponCode);
+        
+    //     $this->addFlash('success', 'Le coupon a été appliqué avec succès!');
+        
+    //     return $this->redirectToRoute('cart_show');
+    // }
+
     
 
 }
--- symfonyv2/src/Controller/HomeController.php
+++ symfonyv2/src/Controller/HomeController.php
@@ -21,7 +21,6 @@ class HomeController extends AbstractController
     #[Route('/proteines', name: 'home_prot')]
     public function showProt(ProductsRepository $productsRepository): Response
     {
-
         $wheyProducts = $productsRepository->findBySlugContainingWhey();
         $bars = $productsRepository->findBar();
 
@@ -35,53 +34,18 @@ class HomeController extends AbstractController
     #[Route('/barre', name: 'home_barre')]
     public function showBar(ProductsRepository $productsRepository): Response
     {
-
         $wheyProducts = $productsRepository->findBySlugContainingWhey();
+        $textForCarrousel = $productsRepository->findAll();
         $bars = $productsRepository->findBar();
         // dd($bars);
 
         return $this->render('produits/barre.html.twig', [
             'controller_name' => 'HomeController',
             'wheyProducts' => $wheyProducts,
-            'bars' => $bars
+            'bars' => $bars,
+            'textForCarrousel' => $textForCarrousel
         ]);
     }
 
-    private function calculateCartTotal(SessionInterface $session, ProductsRepository $productsRepository)
-    {
-        $cart = $session->get('cart', []);
-        $total = 0;
-
-        foreach ($cart as $id => $quantity) {
-            $product = $productsRepository->find($id);
-            if ($product) {
-                $total += ($product->getPrice() * $quantity);
-            }
-        }
-
-        return $total;
-    }
-
-    #[Route('/cart', name: 'cart_show')]
-    public function show(SessionInterface $session, ProductsRepository $productsRepository): Response
-    {
-        $cart = $session->get('cart', []);
-        $cartWithData = [];
-        $total = $this->calculateCartTotal($session, $productsRepository);
-
-        foreach ($cart as $id => $quantity) {
-            $cartWithData[] = [
-                'product' => $productsRepository->find($id),
-                'quantity' => $quantity
-            ];
-        }
-
-        return $this->render('cart/index.html.twig', [
-            'items' => $cartWithData,
-            'total' => $total
-        ]);
-    }
-
-    
 
 }
